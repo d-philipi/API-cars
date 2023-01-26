bb3eed31adc81c4cbb672935474d5bee1b503d4e
Daniel Philipi <daniphilipi379@gmail.com>
2023-01-25 23:27:50 -0300

    Refact: yaer number schema

--- src/schemas/carSchema.ts
+++ src/schemas/carSchema.ts
@@ -3,6 +3,6 @@ import joi from "joi";
 export const carSchema = joi.object({
   model: joi.string().required(),
   licensePlate: joi.string().length(7).required(),
-  year: joi.string().required(),
+  year: joi.number().required(),
   color: joi.string().required()
 });
